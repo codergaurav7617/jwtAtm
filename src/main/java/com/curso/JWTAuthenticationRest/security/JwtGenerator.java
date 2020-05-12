b53402d6d4d80e90e8550bb9b631a22ba4d2dc2e
codergaurav7617 <gj76170@gmail.com>
2020-05-12 10:02:42 +0530

    moved the jwtExpiration to the application.proprties file and read from it

--- src/main/java/com/curso/JWTAuthenticationRest/security/JwtGenerator.java
+++ src/main/java/com/curso/JWTAuthenticationRest/security/JwtGenerator.java
@@ -5,6 +5,7 @@ import com.curso.JWTAuthenticationRest.model.JwtUser;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -12,16 +13,20 @@ import java.util.Date;
 @Component
 public class JwtGenerator {
 
+
+    @Value("${jwtExpiration}")
+    private int jwtExpiration;
+
     public String generate(JwtUser jwtUser) {
         Claims claims = Jwts.claims()
                 .setSubject(jwtUser.getUsername());
         claims.put(Constants.USER_ID, String.valueOf(jwtUser.getId()));
         claims.put(Constants.ROLE, jwtUser.getRole());
-
+    //new Date(System.currentTimeMillis() + 40000)
         return Jwts.builder()
                 .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS256, Constants.YOUR_SECRET)
-                .setExpiration(new Date(System.currentTimeMillis() + 40000))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration*10000))
                 .compact();
     }
 }
